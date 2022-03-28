cf0149c5e1aaa4f4d6c32c0add236ca85a7aa1b9
B-Starkenburg <spamberan@gmail.com>
2022-03-28 12:35:38 +0200

    tuner

--- DQNagent.py
+++ DQNagent.py
@@ -1,8 +1,14 @@
+from msilib.schema import Directory
 import numpy as np
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 from tensorflow.keras.optimizers import Adam
+from kerastuner.tuners import RandomSearch
+from kerastuner.engine.hyperparameters import HyperParameters
+import time
 
+LOG_DIR = f"models/{int(time.time())}"
+#from tensorflow.keras.
 
 
 class DQNagent():
@@ -17,6 +23,14 @@ class DQNagent():
         self.model = self._build_model()
         self.target_model = self._build_model()
     
+        self.tuner = RandomSearch(
+            self._tune_model,
+            objective = "accuracy",
+            max_trials = 3,
+            executions_per_trial = 1,
+            directory = LOG_DIR
+        )
+
     def _build_model(self):
         #Build the neural network, might be better to have it outside of the class for experimentation...
         #Right now its just a simple one with keras but we will have to experiment with number of layers, nodes, activation function, learning rate etc.
@@ -25,7 +39,21 @@ class DQNagent():
         model.add(Dense(24, input_dim=self.state_shape, activation='relu'))
         model.add(Dense(24, activation='relu'))
         model.add(Dense(self.n_possible_actions, activation='linear'))
-        model.compile(loss='mse', optimizer=Adam(learning_rate=self.learning_rate))
+        model.compile(loss='mse', optimizer=Adam(learning_rate=self.learning_rate), metrics=['accuracy'])
+        return model
+
+    def _tune_model(self, hp):
+        #Build the neural network, might be better to have it outside of the class for experimentation...
+        #Right now its just a simple one with keras but we will have to experiment with number of layers, nodes, activation function, learning rate etc.
+
+        model = Sequential()
+        model.add(Dense(hp.Int("input_units", 32, 256, 32), input_dim=self.state_shape, activation='relu'))
+        for i in range(hp.Int("n_layers0", 1, 4)):
+            model.add(Dense(hp.Int(f"dens_{i}_units0", 32, 256, 32), input_dim=self.state_shape, activation='relu'))
+        
+        
+        model.add(Dense(self.n_possible_actions, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(learning_rate=self.learning_rate), metrics=['accuracy'])
         return model
     
     def action_selection(self, state, method ="egreedy"):
@@ -68,8 +96,7 @@ class DQNagent():
             target = reward + self.gamma*np.max(self.forward_pass(next_state, use_target_network = use_target_network)[0]) #correct? not DDQN right (greedy action from original model)
         Q_values_target[0][action] = target
 
-        #With keras the backpropagation is done automatically with the fit() command
-        self.model.fit(state, Q_values_target, epochs = 1, verbose = 0)
+        self.tuner.search(x = state, y = Q_values_target, epochs = 1, batch_size = 64)
     
     def memorize(self, state, action, reward, next_state, done):
         
